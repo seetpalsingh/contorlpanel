281f8abdd2762e3f0dbd003115357a4cc28a56d6
Seetpal singh <creators.guru@gmail.com>
2013-05-26 09:25:59 -0700

    done for today
    todays last update rest of thing is tags, color pallet output, and bg
    opacity combines.

--- editor.js
+++ editor.js
@@ -9,10 +9,25 @@ $(function() {
 		iframeFix: true
 	});
 
-	$( "#slider-range" ).slider();
+	$( "#slider-range" ).slider({
+		value:100,
+		min: 0,
+		max: 101,
+		slide: function( event, ui ) {
+//			console.log(ui.value);
+			$( "#result-board" ).css({ background: 'rgba(46,47,49,'+(ui.value/100)+')'});
+		}
+	});
 	
 	
-	$('#bg-color, #text-color').minicolors({ defaultValue: '#09a35f'});
+	$('.pallet').minicolors({ 
+		defaultValue: '#09a35f',
+		change: function(hex, rgba){
+			var rgba=$( "#result-board" ).css('background-color');
+			var alpha=rgba.replace(/^.*,(.+)\)/,'$1')
+	console.log(alpha);
+		}
+	});
 	
 	
 	$('#cm-editor input[type="text"]').change(function(){
@@ -29,7 +44,7 @@ $(function() {
 		var _val	=	$(this).attr('alt');
 		
 		console.log('#'+_id, _val);
-		$('#'+_id).html(_val);
+		$('#out_'+_id).html(_val);
 		
 	});
 	
